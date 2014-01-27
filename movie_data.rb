68170ae8655d19dd4293448de236bc3743609ab6
Chi Ieong (Eddie) Tai <chiieong@brandeis.edu>
2014-01-26 21:02:29 -0500

    Added PA2 methods to original PA1

--- movie_data.rb
+++ movie_data.rb
@@ -17,17 +17,26 @@ class MovieData
   SCALEAVERAGETIMESTAMP = 10000000000 #2 years gives about 6 rating to popularity formula
     
   #initializes Hashes
-  def initialize
+  def initialize(arg1 = nil,arg2 = nil)
+    
+    
     @user_hash = Hash.new {|h,k| h[k] = Hash.new } #hash where user maps to a hash that maps movie_id to its rating by the user
     @number_of_ratings_hash = Hash.new(0) #hash that maps a movie to the number of rating it has
     @timestamp_hash = Hash.new {|h,k| h[k] = Array.new } #hash that maps movie id to an array of its timestamps of when it was rated.
     @avg_timestamp_hash = Hash.new #hash that maps the movie_id to the average timestamp of when it was rated (see popularity_list())
     @popularity_hash = Hash.new #hash that maps movie_id to its popular given by the formula (Average timestamp/ SCALEAVERAGETIMESTAMP) + number of ratings
     @similarity_hash = Hash.new(0) #hash that maps a user to a similarity score of another user, similarity = (1 / 1 + ((rating of user1) - (rating of user2).abs))
+    @test_set = Hash.new {|h,k| h[k] = Hash.new } 
+    @arg1 = arg1
+    @arg2 = arg2
+    
   end
   
   #returns void
   def load_data()
+        
+    if @arg1 == "ml-100k" and @arg2 == nil
+ 
      File.readlines('u.data').each do |line|
        
        #user_id- 0, movie_id - 1, rating - 2, timestamp - 3
@@ -43,7 +52,115 @@ class MovieData
        @timestamp_hash[Integer(dataLine[1])] << (Integer(dataLine[3]))
   
      end
+    
+    
+    elsif arg1 == 'ml-100k' and arg2 == :u1
+      
+      
+       lines = File.readlines('u1.base')[0..79999] 
+       lines2 = File.readlines('u1.test')[0..19999]
+    
+       lines.each do |value|
+          each_line = value.split(" ")
+          #maps the user hash to a hash of movie_id mapped to the rating given by the user
+          @user_hash[Integer(each_line[0])][Integer(each_line[1])] = (Integer(each_line[2]))
+       end 
+       
+       lines2.each do |value|
+          each_line2 = value.split(" ")
+          #maps the user hash to a hash of movie_id mapped to the rating given by the user
+          @test_set[Integer(each_line2[0])][Integer(each_line2[1])] = (Integer(each_line2[2]))
+       end  
+      
+      
+     else
+    
+        p "Invalid arguments"
+      
+     end
+       
+      
   end
+  
+  
+    def rating(u,m)
+  
+    if @user_hash[u][m] == nil
+      
+      return 0 
+      
+    else
+      
+      return @user_hash[u][m]  
+      
+    end
+    
+    
+  
+  end
+
+
+  def predict(u,m)
+    
+    most_similar_users = most_similar(u)
+    
+    #p most_similar_users
+    
+    total_ratings = 0.0
+    number_of_ratings = 0.0
+    
+    most_similar_users.each do |key|
+    
+      #p @user_hash[key][m]
+      if @user_hash[key][m] != nil
+        total_ratings += @user_hash[key][m]
+        number_of_ratings += 1
+      end
+    
+    end
+    
+    if(number_of_ratings != 0.0)
+    return total_ratings/number_of_ratings
+    else
+    return 0.0
+    end
+    
+  end
+
+  def movies(u)
+    p @user_hash[u].keys
+  end
+  
+  def viewers(m)
+
+    users = Array.new
+    @user_hash.each do |key,value|
+         if(value.has_key?(m))
+        
+         users << key
+          
+         end   
+          
+    end
+    
+    return users
+    
+  end
+
+def run_test(k)
+  
+  result_object = MovieTest.new
+  
+  for x in 0..(k-1)
+    
+    dummy = @ratings[x].split(" ")
+    result_object.store_result(dummy[0],dummy[1],dummy[2],predict(Integer(dummy[0]),Integer(dummy[1])))
+  
+  end
+  
+  return result_object
+  
+end
   
   #takes movie_id (integer) as parameters
   #returns a number determining the popularity
